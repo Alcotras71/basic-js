faaadbd05a603064517b193ec684a04fe344a9c3
Andrey <aimdragman2@yandex.ru>
2021-03-08 15:51:18 +0300

    refactor: change method flat on cycle for

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-	if (!matrix || matrix == undefined) {
+  if (!matrix || matrix == undefined) {
     return 0;
   }
-  const flatMatrix = matrix.flat();
   const newArr = [];
-  for (let cat of flatMatrix) {
-    if (cat == '^^') {
-      newArr.push(cat)
+  for (let mat of matrix) {
+    for (let cat of mat) {
+      if (cat == "^^") {
+        newArr.push(cat);
+      }
     }
   }
-  return newArr.length
+  return newArr.length;
 };
