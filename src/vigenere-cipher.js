609e448a504d0558a7d601e4bb10e4ff437f1f08
Andrey <aimdragman2@yandex.ru>
2021-03-29 05:05:45 +0300

    refactor: add more and more optimization

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,7 +4,6 @@ class VigenereCipheringMachine {
   constructor(isDirect = true) {
     this.isDirect = isDirect;
     this.alphabet = "";
-    this.tabulaRecta = [];
   }
   createAlphabet() {
     for (let i = 65; i <= 90; i++) {
@@ -12,109 +11,39 @@ class VigenereCipheringMachine {
     }
   }
 
-  createTabulaRecta() {
-    this.createAlphabet();
-    for (let i = 0; i < this.alphabet.length; i++) {
-      let row = this.alphabet.slice(i);
-      row += this.alphabet.slice(0, i);
-      this.tabulaRecta.push(row);
-    }
-  }
+	encrypt(message, key) {
+		this.createAlphabet();
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    let position = 0;
 
-  equalizeLength(message, key) {
-    key = key.toUpperCase();
-    message = message.toUpperCase();
-    const messageArr = message.split("");
-    let newKey = "";
-    while (newKey.length < messageArr.length) {
-      newKey += key;
-    }
-    return {
-      newKey: newKey,
-      newMessage: message,
-    };
-  }
-
-  encrypt(message, key) {
-    if (!message || !key) {
-      throw new Error("Message or key is not passed");
-    } else {
-      this.createTabulaRecta();
-      const result = this.equalizeLength(message, key);
-      message = result.newMessage;
-      key = result.newKey;
-      let encryptSolution = [];
-
-      for (let i = 0, j = 0; i < message.length; i++, j++) {
-        let elM = message[i],
-          elK = key[j];
-        if (elM == " ") {
-          encryptSolution.push(elM);
-          j--;
-        } else {
-          if (this.alphabet.indexOf(elM) >= 0) {
-            let m = this.alphabet.indexOf(elM);
-            let k = this.alphabet.indexOf(elK);
-
-            encryptSolution.push(this.tabulaRecta[m][k]);
-          } else {
-            encryptSolution.push(message[i]);
-          }
-        }
+    let result = message.map(item => {
+      if (this.alphabet.includes(item)) {
+        let index = (this.alphabet.indexOf(key[position]) + this.alphabet.indexOf(item)) % this.alphabet.length;
+        position = (position + 1) % key.length;
+        return this.alphabet[index];
       }
-
-      encryptSolution = encryptSolution.join("");
-      if (this.isDirect) {
-        return encryptSolution;
-      } else {
-        const reverseEncryptSolution = encryptSolution
-          .split("")
-          .reverse()
-          .join("");
-        return reverseEncryptSolution;
-      }
-    }
+      return item;
+    });
+    return this.isDirect ? result.join('') : result.reverse().join('');
   }
 
   decrypt(message, key) {
-    if (!message || !key) {
-      throw new Error("Message or key is not passed");
-    } else {
-      this.createTabulaRecta();
-      const result = this.equalizeLength(message, key);
-      message = result.newMessage;
-      key = result.newKey;
-      let decryptSolution = [];
+		this.createAlphabet();
+    message = message.toString().toUpperCase().split('');
+    key = key.toString().toUpperCase().split('');
+    let position = 0;
 
-      for (let i = 0, j = 0; i < message.length; i++, j++) {
-        let elM = message[i],
-          elK = key[j];
-        if (elM == " ") {
-          decryptSolution.push(elM);
-          j--;
-        } else {
-          if (this.alphabet.indexOf(elM) >= 0) {
-            let m = this.alphabet.indexOf(elK);
-            let k = this.tabulaRecta[m].indexOf(elM);
-
-            decryptSolution.push(this.alphabet[k]);
-          } else {
-            decryptSolution.push(elM);
-          }
-        }
+    let result = message.map(item => {
+      if (this.alphabet.includes(item)) {
+        let index = this.alphabet.indexOf(item) - this.alphabet.indexOf(key[position]);
+        if (index < 0) index += this.alphabet.length;
+        position = (position + 1) % key.length;
+        return this.alphabet[index];
       }
-      decryptSolution = decryptSolution.join("");
-
-      if (this.isDirect) {
-        return decryptSolution;
-      } else {
-        const reverseDecryptSolution = decryptSolution
-          .split("")
-          .reverse()
-          .join("");
-        return reverseDecryptSolution;
-      }
-    }
+      return item;
+    });
+    return this.isDirect ? result.join('') : result.reverse().join('');
   }
 }
 
